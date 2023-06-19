143a3fbcad403f89b3a346cd40efa7e40041b648
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-19 16:17:52 +0200

    setting files and general.scss in style folder

---
+++ src/components/CardsList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+import SingleCard from './SingleCard.vue';
+
+export default {
+    components: {
+        SingleCard,
+    }
+}
+</script>
+
+<style lang="scss">
+    
+</style>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SingleCard",
+}
+</script>
+
+<style lang="scss">
+    
+</style>
