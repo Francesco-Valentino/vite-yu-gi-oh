d76d8fd7841792640e70eb2d7b69356e8d1a911e
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-19 16:38:52 +0200

    setting header basic structure

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
+    <div class="upperHeader">
+        <img src="../assets/img/ygoLogo.png" alt="Yu-Gi-Oh logo">
+
         <h1>
-            HEADER
+            YU-GI-OH API
         </h1>
+    </div>
+
+    <div class="lowerHeader">
+
     </div>
 </template>
 
@@ -13,5 +19,23 @@
     </script>
 
 <style lang="scss" scoped>
-    
+
+    div.upperHeader,
+    div.lowerHeader{
+        height: 70px;
+    }
+    div.upperHeader{
+        display: flex;
+        justify-content: start;
+        align-items: center;
+
+        img{
+            height: 70px;
+            padding: 0 1.5rem;
+        }
+    }
+
+    div.lowerHeader{
+        background-color: #D48F38;
+    }
 </style>
